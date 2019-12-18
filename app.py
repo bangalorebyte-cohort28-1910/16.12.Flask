78596b08c5a09096101aad9fe562e7f34eb56d0b
datapsyche <jithinjkumar@gmail.com>
2019-12-18 10:32:58 +0530

    added flask

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello Flask!'
+    if request.method == 'POST':
+        print("post request submitted") 
+        name = request.form['name']
+        user = User(name=name)
+        db.session.add(user)
+        db.session.commit()
+        name_list = User.query.all()
+        return render_template('index.html',name_list=name_list)
+    elif request.method == 'GET':
+        print("get request is asked")
+        return render_template('index.html')
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     return 'About US page'
 
@@ -15,4 +36,6 @@ def contact():
     return 'Contact Us page'
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
